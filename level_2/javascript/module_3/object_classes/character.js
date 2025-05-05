dbb72742ce5b3d92a064bc72cd915f8f35971d13
Konstantin Georgiev <konsstantin.georgiev@gmail.com>
2025-05-05 15:06:43 +0100

    object class method demo

--- level_2/javascript/module_3/object_classes/character.js
+++ level_2/javascript/module_3/object_classes/character.js
@@ -1,17 +1,39 @@
-class Character {
-    constructor(name, health, strength, hairColor, clothesColor, goldCoins) {
-        this.name = name;
-        this.health = health;
-        this.strength = strength;
-        this.hairColor = hairColor;
-        this.clothesColor = clothesColor;
-        this.goldCoins = goldCoins;
+// class Character {
+//     constructor(name, health, strength, hairColor, clothesColor, goldCoins) {
+//         this.name = name;
+//         this.health = health;
+//         this.strength = strength;
+//         this.hairColor = hairColor;
+//         this.clothesColor = clothesColor;
+//         this.goldCoins = goldCoins;
+//     }
+// }
+
+// const player = new Character("HeMan", 100, 20, "yellow", "brown", 0);
+// const enemy = new Character("Skeletor", 80, 15, "black", "purple", 100);
+
+// console.log(player);
+// console.log(enemy);
+// console.log(player.name);
+
+
+class Chest {
+    constructor(isOpen, coins) {
+        this.isOpen = isOpen;
+        this.coins = coins;
+    }
+
+    openChest() {
+        if (!this.isOpen) {
+            this.isOpen = true;
+            console.log(`Chest opened! Collected ${this.coins} coins!`);
+            this.coins = 0;
+        }
     }
 }
 
-const player = new Character("HeMan", 100, 20, "yellow", "brown", 0);
-const enemy = new Character("Skeletor", 80, 15, "black", "purple", 100);
+const myChest = new Chest(false, 100);
 
-console.log(player);
-console.log(enemy);
-console.log(player.name);
+myChest.openChest();
+
+console.log(myChest.coins);
