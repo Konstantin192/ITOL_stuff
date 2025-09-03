4310fec649586f31dc3440118cbc089878e45278
Konstantin Georgiev <konsstantin.georgiev@gmail.com>
2025-09-03 22:59:26 +0100

    added menu options for recipe instruction addition, removal and edit added menu option for recipe deletion

--- level_3/Python/recipe_manager/menus.py
+++ level_3/Python/recipe_manager/menus.py
@@ -6,7 +6,8 @@ def main_menu(recipe_list):
     print("2. Add Recipes")
     print("3. Edit Recipes")
     print("4. Delete Recipes")
-    print("5. Exit")
+    print("5. Search Recipes")
+    print("6. Exit")
 
     option_selected = int(input("\nWhat would you like to do? : "))
 
@@ -17,6 +18,7 @@ def main_menu(recipe_list):
             recipe_list[selected_recipe - 1].view_recipe_details()
         case 2:
             core_functions.add_recipe(recipe_list)
+        # ToDo split into smaller functions
         case 3:
             core_functions.view_recipes(recipe_list)
             selected_recipe = int(input("\nSelect recipe to edit: "))
@@ -45,3 +47,25 @@ def main_menu(recipe_list):
                             recipe_list[selected_recipe - 1].remove_ingredients()
                         case 3:
                             recipe_list[selected_recipe - 1].change_ingredients()
+
+                case 3:
+                    print("\nInstruction edit options:")
+                    print("1. Add instructions")
+                    print("2. Remove instructions")
+                    print("3. Change instructions")
+                    selected_instruction_edit = int(input("\nWhat would you like to do? : "))
+
+                    match selected_instruction_edit:
+                        case 1:
+                            recipe_list[selected_recipe - 1].add_instructions()
+                        case 2:
+                            recipe_list[selected_recipe - 1].remove_instructions()
+                        case 3:
+                            recipe_list[selected_recipe - 1].change_instructions()
+        case 4:
+            print("\nCurrent recipes")
+            core_functions.view_recipes(recipe_list)
+            selected_recipe_delete = int(input("\nWhich would you like to delete? : "))
+
+            core_functions.delete_recipe(recipe_list, selected_recipe_delete - 1)
+
