7da61dd4fab5917202debc7b73d23f68603184b1
Konstantin Georgiev <konsstantin.georgiev@gmail.com>
2025-09-15 19:05:07 +0100

    replaced input validation in change_ingredients function with new validation function

--- level_3/Python/recipe_manager/recipe.py
+++ level_3/Python/recipe_manager/recipe.py
@@ -104,7 +104,7 @@ class Recipe:
     def change_ingredients(self):
         valid_inputs = [1, 2]
 
-        print("Current ingredient list: ")
+        print("\nCurrent ingredient list: ")
         self.view_ingredients()
         change_more_ingredients = True
 
@@ -113,37 +113,26 @@ class Recipe:
             user_input_validated = False
 
             while not ingredient_input_validated:
-                selected_ingredient = input("\nSelect index of ingredient to change: ")
-                selected_ingredient_is_int = (selected_ingredient).isdigit()
+                selected_ingredient = input("\nSelect ingredient to change: ")
 
-                if selected_ingredient_is_int:
-                    selected_ingredient = int(selected_ingredient)
+                ingredient_input_validated = input_validation.recipe_input_validation(selected_ingredient, self.ingredients_list)
 
-                    if selected_ingredient > 0 and selected_ingredient <= len(self.ingredients_list):
-                        ingredient_input_validated = True
-                    else:
-                        print("\nInvalid input. Please try again.")
+                if ingredient_input_validated:
+                    new_ingredient = input("\nEnter new ingredient: ")
+                    self.ingredients_list[int(selected_ingredient) - 1] = new_ingredient
+
+                    print("\nNew ingredient list:")
+                    self.view_ingredients()
                 else:
                     print("\nInvalid input. Please try again.")
 
-            new_ingredient = input("\nEnter new ingredient: ")
-
-            self.ingredients_list[int(selected_ingredient) - 1] = new_ingredient
-
-            print("\nNew ingredient list:")
-            self.view_ingredients()
-
             while not user_input_validated:
                 user_input = input("\nWould you like to change another ingredient?: \n1. Yes \n2. No \n")
-                user_input_is_int = user_input.isdigit()
 
-                if user_input_is_int:
-                    user_input = int(user_input)
+                user_input_validated = input_validation.menu_input_validation(user_input, valid_inputs)
 
-                if user_input in valid_inputs:
-                    user_input_validated = True
-
-                    if user_input == 2:
+                if user_input_validated:
+                    if int(user_input) == 2:
                         change_more_ingredients = False
                 else:
                     print("\nInvalid input. Please try again.")
